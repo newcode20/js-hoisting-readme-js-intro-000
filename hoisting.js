353721f1c12fcae3af376acf05fc7716dac41e69
stellar-function-e8f8 <kwooten1993@gmail.com>
2021-03-28 00:56:09 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -18,12 +18,13 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
-
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
+  var name = "Cricky";
+
+  sayMy();
+
+  
 }
